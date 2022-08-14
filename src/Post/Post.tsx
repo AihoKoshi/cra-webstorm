1902783bbcd1a430300adb5d9c3ad71197478096
Aiko Hoshi <aihokoshi@gmail.com>
2022-08-14 17:14:52 +0200

    Commit - 4

--- src/Post/Post.tsx
+++ src/Post/Post.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 export const Post = () => {
     return (
         <div>
-            <h2>Post333</h2>
+            <h2>Post1</h2>
+            <h2>Post2</h2>
+            <h2>Post3</h2>
+            <h2>Post4</h2>
         </div>
     )
 }
--- src/User/User.tsx
+++ src/User/User.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 export const User = () => {
     return (
         <div>
-            <h2>User444</h2>
+            <h2>User1</h2>
+            <h2>User2</h2>
+            <h2>User3</h2>
+            <h2>User4</h2>
         </div>
     )
 }
